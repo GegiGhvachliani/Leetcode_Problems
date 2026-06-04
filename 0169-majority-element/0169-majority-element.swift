186e852080d4b20cf830e8e4d067c50ce2f80f79
Gegi Ghvachliani <gegighvachliani@gmail.com>
2026-06-04 14:24:49 +0400

    Time: 0 ms (100%), Space: 19.1 MB (96.63%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var candidate = 0
+        var count = 0
+
+        for num in nums {
+            if count == 0 {
+                candidate = num
+            }
+
+            if num == candidate {
+                count += 1
+            } else {
+                count -= 1
+            }
+        }
+
+        return candidate
+    }
+}
