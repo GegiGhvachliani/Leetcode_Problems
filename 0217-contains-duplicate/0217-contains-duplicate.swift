9bfa8a75e381e2d8ebfa351320ced7e7e4634693
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-06-09 15:37:19 +0400

    Time: 12 ms (81.23%), Space: 22.5 MB (53.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.swift
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -1,11 +1,5 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        var setValue = Set(nums)
-
-        if setValue.count < nums.count {
-            return true
-        }
-        
-        return false
+        return Set(nums).count != nums.count
     }
 }
