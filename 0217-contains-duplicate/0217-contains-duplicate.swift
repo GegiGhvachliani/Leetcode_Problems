562a42dfba79bc07c6ce52ba26b3762ff881d496
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-06-09 15:35:24 +0400

    Time: 9 ms (86.71%), Space: 22.3 MB (81.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.swift
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -1,11 +1,5 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
-        var dict: [Int:Int] = [:]
-        
-        for i in nums {
-            dict[i, default: 0] += 1
-            if dict[i]! > 1 { return true }
-        }
-        return false
+        return Set(nums).count != nums.count
     }
 }
