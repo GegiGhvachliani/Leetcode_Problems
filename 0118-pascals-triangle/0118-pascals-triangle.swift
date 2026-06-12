756e8e7a545850a38617e90dceb99f50a75eea09
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-06-12 23:44:20 +0400

    Time: 0 ms (100%), Space: 19.4 MB (95.6%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.swift
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -1,13 +1,14 @@
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
         var rows: [[Int]] = [[1]]
+        rows.reserveCapacity(numRows)
         
-        for i in  1..<numRows {
-            var p = Array(repeating: 1, count: i+1)
+        for i in 1..<numRows {
+            var row = Array(repeating: 1, count: i + 1)
             for j in 1..<i {
-                p[j] = rows[i-1][j] + (rows[i-1][j - 1] ?? 0)
+                row[j] = rows[i-1][j] + rows[i-1][j-1]
             }
-            rows.append(p)
+            rows.append(row)
         }
         return rows
     }
