3210c7afa7ef0ebfef7a2d2dfc145efa3ac291bc
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-06-20 15:30:54 +0400

    Time: 11 ms (62.36%), Space: 22.9 MB (27.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.swift
+++ 0049-group-anagrams/0049-group-anagrams.swift
@@ -3,19 +3,13 @@ class Solution {
         guard strs.count > 1 else { return [strs] }
         
         var anagrams: [[Character]: [String]] = [:]
-        
-        var groupedAnagrams: [[String]] = []
-        
+                
         for word in strs {
             let k = word.sorted()
             anagrams[k, default: []].append(word)
         }
         
-        for i in anagrams.keys {
-            groupedAnagrams.append(anagrams[i]!)
-        }
-        
-        return groupedAnagrams
+        return Array(anagrams.values)
     }
 }
 
