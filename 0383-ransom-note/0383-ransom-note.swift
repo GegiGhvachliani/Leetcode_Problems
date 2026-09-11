77dc7313eb45d5c8c3acdd8d6c9e2ae971d352c1
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-09-11 17:50:47 +0400

    Time: 25 ms (56.72%), Space: 20 MB (52.05%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        
+        var dict: [Character:Int] = [:]
+        
+        for char in magazine {
+            dict[char, default: 0] += 1
+        }
+        
+        for char in ransomNote {
+
+            dict[char, default: 0] -= 1
+            
+            if (dict[char]!) < 0 {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
