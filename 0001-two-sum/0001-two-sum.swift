58cab6e2c9704c0ad681de0a33e3a977f2a70894
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-09-11 19:06:15 +0400

    Time: 4 ms (34.21%), Space: 19.8 MB (56.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,14 +1,17 @@
 class Solution {
-     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-         var dict: [Int:Int] = [:]
-         
-         for i in 0..<nums.count {
-             if let j = dict[target - nums[i]] {
-                 return [j, i]
-             }
-             dict[nums[i]] = i
-         }
-         
-         return []
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+
+        var dict: [Int:Int] = [:]
+        
+        for i in 0..<nums.count {
+            
+            if let k = dict[target - nums[i]] {
+                return [k, i]
+            } else {
+                dict[nums[i]] = i
+            }
+        }
+        
+        return []
     }
 }
