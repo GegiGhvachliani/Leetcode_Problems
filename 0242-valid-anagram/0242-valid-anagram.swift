f9cec29bbc2a40e302a82ff9fd0f9202ab25a5e2
Gegi Ghvachliani <230540979+GegiGhvachliani@users.noreply.github.com>
2026-09-11 17:35:10 +0400

    Time: 11 ms (76.61%), Space: 20.2 MB (46.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.swift
+++ 0242-valid-anagram/0242-valid-anagram.swift
@@ -1,18 +1,23 @@
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-    guard s.count == t.count else { return false }
-
-    var charCounts: [Character: Int] = [:]
-    
-    for char in s {
-        charCounts[char, default: 0] += 1
+        
+        var s1: [Character] = Array(s)
+        var s2: [Character] = Array(t)
+        
+        guard s1.count == s2.count else { return false }
+        
+        var dict1: [Character:Int] = [:]
+        var dict2: [Character:Int] = [:]
+                        
+        for char in s1 {
+            dict1[char, default: 0] += 1
+        }
+        
+        for char in s2 {
+            dict2[char, default: 0] += 1
+        }
+        
+        return dict1 == dict2
     }
-    
-    for char in t {
-        guard let count = charCounts[char], count > 0 else { return false }
-        charCounts[char] = count - 1
-    }
-   
-    return true
-  }
 }
+
